a08dd692ed8c86c5f7cdf483fabd0a6f190cba14
Vedant <vedantsagar12@gmail.com>
2022-01-22 23:08:52 +0530

    Getting data from server

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = 8000;
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded());
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static("assests"));
 
@@ -12,6 +13,11 @@ app.get("/", function (req, res) {
   return res.render("home", {
     title: "To-Do-list",
   });
+});
+
+app.post("/create-task", function (req, res) {
+  console.log(req.body);
+  res.redirect("back");
 });
 
 app.listen(port, function (err) {
